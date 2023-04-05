85f76d91a4a4e5936ceb8e777cb0cf672b138c09
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-05 23:34:09 +0900

    Add: HomeScreen AppBar 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
       body: Center(
         child: Text('Home Screen'),
       ),
