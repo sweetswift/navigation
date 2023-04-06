815cffbf4c94b90c46524ae8e253a02b769e2b44
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-06 20:47:47 +0900

    Add: push,pop Argument 사용해보기

--- lib/screen/route_one_screen.dart
+++ lib/screen/route_one_screen.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
-  const RouteOneScreen({Key? key}) : super(key: key);
+  final int number;
+
+  const RouteOneScreen({
+    required this.number,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MainLayout(
       title: 'Route One',
       children: [
+        Text(
+          'arguments : ${number.toString()}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => RouteTwoScreen(),
+                settings: RouteSettings(
+                  arguments: 789,
+                ),
+              ),
+            );
+          },
+          child: Text('push'),
+        ),
         ElevatedButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          child: Text('pop'),
+          child: Text('Pop'),
         ),
       ],
     );
--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+
     return MainLayout(
       title: 'Route Two',
       children: [
+        Text(
+          'arguments: ${arguments}',
+          textAlign: TextAlign.center,
+        ),
         ElevatedButton(
           onPressed: () {
             Navigator.of(context).pop();
@@ -18,7 +25,8 @@ class RouteTwoScreen extends StatelessWidget {
         ElevatedButton(
           onPressed: () {
             Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => RouteTwoScreen(),
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
               ),
             );
           },
